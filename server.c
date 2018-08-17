a7341ed444f8defff4991bee230f5dcfa814a2f8
rishsome <rishi580pal@gmail.com>
2018-08-17 16:17:07 +0530

    first commit

---
+++ server.c
@@ -0,0 +1,24 @@
+/* A simple server in the internet domain using TCP 
+  The Port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void error(const char *msg)
+{
+	perrror(msg);
+	exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+	int sockfd, newsockfd, portno;
+	socklen_t, clilen;
+	char buffer[225];
+	struct sockaddr_in serv_addr, cli_addr; 
+	
+} 
