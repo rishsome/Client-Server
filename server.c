80995bc2150d9b5a9e87ea1dff62205d8eb49c35
rishsome <rishi580pal@gmail.com>
2018-08-20 18:52:20 +0530

    client included

--- server.c
+++ server.c
@@ -37,5 +37,34 @@ int main(int argc, char *argv[])
 
 	if(bind(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 		error("Binding Failed");
-		     
+
+	listen(sockfd, 5);
+	clilen= sizeof(cli_addr);
+
+	newsockfd =accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+	if(newsockfd < 0)
+		error("ERROR on Accept");
+
+	while(1)
+	{
+		bzero(buffer, 256);
+		n= read(newsockfd, buffer, 255);
+		if(n<0)
+			error("Error on reading");
+		printf("CLient : %s\n", buffer);
+		bzero(buffer, 255);
+		fgets(buffer, 255, stdin);
+
+		n= write(newsockfd, buffer, strlen(buffer));
+		if(n<0)
+			error("Error on writing.");
+		int i= strncmp("Bye", buffer, 3);
+		if(i==0)
+			break;
+	}
+	close(newsockfd);
+	close(sockfd);
+	return 0
+;
  } 
