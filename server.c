0f87a22448c4bc9b2f4c9ccc347204e05c23c188
rishsome <rishi580pal@gmail.com>
2018-08-20 18:10:53 +0530

    vhj

--- server.c
+++ server.c
@@ -19,7 +19,23 @@ int main(int argc, char *argv[])
 	int sockfd, newsockfd, portno;
 	socklen_t, clilen;
 	char buffer[225];
-	struct sockaddr_in serv_addr, cli_addr; 
+	struct sockaddr_in serv_addr, cli_addr;
+	int n;
+	if(argc < 2)
+	{
+		fprintf(stderr, "ERROR, no port provided\n");
+		exit(1);
+	}
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0)
+		error("ERROR opening socket");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno= atoi(argv[1]);
+	serv_addr.sin_family= AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
 
-
-} 
+	if(bind(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+		error("Binding Failed");
+		     
+ } 
